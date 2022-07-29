1743b620d17a6b493af076830cf2a0c06c350f18
Faith-qa <atienofaith12@gmail.com>
2022-07-29 18:52:05 +0300

    fixed errors and updated yaml file

--- aws-serverless-Todoapp/client/src/config.ts
+++ aws-serverless-Todoapp/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'mczva6z5x0'
+const apiId = '099os4e97d'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
