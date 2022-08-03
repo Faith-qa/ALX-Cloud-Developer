9e1bd068f9246d82a457ea223032f78b3c3af10c
Faith-qa <atienofaith12@gmail.com>
2022-08-03 16:39:42 +0300

    corrected authentication and lambda functions

--- aws-serverless-Todoapp/backend/src/lambda/auth/auth0Authorizer.ts
+++ aws-serverless-Todoapp/backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,10 +1,10 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify,} from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+//import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
@@ -55,33 +55,25 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
-  //const alg = jwt.header.alg
-  const kid = jwt.header.kid
-
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
   const response = await Axios.get(jwksUrl)
   const jwks = response.data.keys
-  logger.info('jwks: ' + jwks)
 
-  const jwk = jwks[0]
-  logger.info('jwk: ' + jwk)
+  try {
 
-  if (kid !== jwk.kid) throw new Error('Key mismatch')
+    const token = getToken(authHeader)
 
-  const cert =
-    '-----BEGIN CERTIFICATE-----\n' + jwk.x5c[0] + '\n-----END CERTIFICATE-----'
-
-  logger.info('cert:\n' + cert)
-
-  return verify(
-    token,
-    cert,
-    { algorithms: ['RS256'] }
-  ) as JwtPayload
+    // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
+    const pemData = jwks[0]['x5c'][0]
+    
+    return verify(token, certToPEM(pemData), { algorithms: ['RS256'] }) as JwtPayload
+  } catch(err){
+    logger.error('Fail to authenticate', err)
+  }
+}
+function certToPEM(cert: string) {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cert;
 }
 
 function getToken(authHeader: string): string {
--- aws-serverless-Todoapp/client/src/config.ts
+++ aws-serverless-Todoapp/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'swz7gpmzhf'
+const apiId = 'g68dauh4uh'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-wg5i5b-c.us.auth0.com',            // Auth0 domain
-  clientId: 'jfXrIryt5eLC4JIw7q4J8FVmieTPNlY5',          // Auth0 client id
+  clientId: 'JgPgsDRjuR1unem4sH9WgdwkVOg8C4ca',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
